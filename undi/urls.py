ea3275a20119acae22600d10312a4c44c7d517b4
zahiruddin <kepaknaga@gmail.com>
2020-02-20 22:29:10 +0800

    tutorial part 3

--- undi/urls.py
+++ undi/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-          path('', views.index, name='index'),
+          #path('', views.index, name='index'),
           path('question/', views.question_view, name='question_view'),
+          # ex: /polls/
+          path('', views.index, name='index'),
+          # ex: /polls/5/
+          path('<int:question_id>/', views.detail, name='detail'),
+          # ex: /polls/5/results/
+          path('<int:question_id>/results/', views.results, name='results'),
+          # ex: /polls/5/vote/
+          path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
 ]
--- undi/views.py
+++ undi/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 def index(request):
-          return HttpResponse("hoi hello sini tempat undi weh!")
+           latest_question_list = Question.objects.order_by('-pub_date')[:5]
+           template = loader.get_template('undi/index.html')
+           context = {
+                     'latest_question_list':latest_question_list,
+           }
+           return render(request, 'undi/index.html', context)
+           #return HttpResponse(template.render(context, request))
+
+#def index(request):
+ #         latest_question_list = Question.objects.order_by('-pub_date')[:5]
+ #         output = ', '.join([q.question_text for q in latest_question_list])
+ #         return HttpResponse(output)
+
+#def index2(request):
+ #         return HttpResponse("hoi hello sini tempat undi weh!")
+
+def detail(request, question_id):
+          return HttpResponse("you are looking at question %s." % question_id)
+
+def results(request, question_id):
+          response = "you are looking at the same result of question %s."
+          return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+          return HttpResponse("You're voting on question %s." % question_id)
 
 def question_view(request):
           question = Question.objects.all()
