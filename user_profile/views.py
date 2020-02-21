061a46f3459ce457e29bd48fb53926a0350aec51
zahiruddinnorzain <kepaknaga@gmail.com>
2020-02-21 23:44:34 +0800

    done profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect
 from register.models import Register
 
 def index(request):
-	context = {
-		'posts': Register.objects.all()
-	}
-	return render(request, 'undi/profile.html', context)
+    user = Register.objects.filter(ic_text='123').first() #123 tu ganti dengan session
+    '''
+    user_ic = user.ic_text
+    user_sex = user.sex
+    user_monthly_salary_text = user.monthly_salary_text
+    '''
+    context = {
+        'posts': user,
+    }
+    return render(request, 'undi/profile.html', context)
